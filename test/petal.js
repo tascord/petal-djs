f541ea53d999a907d574d55d5a2927b3b0a1dea2
tascord <theactivesquare@gmail.com>
2021-06-05 13:05:04 +1000

    oops...

--- test/petal.js
+++ test/petal.js
@@ -1,7 +1,7 @@
 const { Petal } = require('../dist');
 
 new Petal({
-    token: 'NzMwOTY0OTY5NDk5MTMxOTk1.XwfKLQ.irE8p4QPU33HK7RFWpTDZ_Pt7ZQ',
+    token: 'your token here!',
     module_location: require('path').join(__dirname, 'modules'),
     database_location: require('path').join(__dirname, 'petal.sqlite')
 })
