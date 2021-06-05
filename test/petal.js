97b7a8e3ddb49105a023ccfd65ab47cafc78e0a2
tascord <theactivesquare@gmail.com>
2021-06-05 19:40:14 +1000

    Version rollover

--- test/petal.js
+++ test/petal.js
@@ -1,7 +1,7 @@
 const { Petal } = require('../dist');
 
 new Petal({
-    token: 'ODQ4NzY4NDg0Njg1OTA1OTUw.YLRbQA.6q4YlDeXDFMJQXv2ouNdl71yZYI',
+    token: 'your token here!',
     module_location: require('path').join(__dirname, 'modules'),
     database_location: require('path').join(__dirname, 'petal.sqlite')
 })
