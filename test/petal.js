0d24f1daf4c71dc943179000040c7f144a829c19
tascord <theactivesquare@gmail.com>
2021-06-05 16:56:16 +1000

    Update discord.js dev branch

--- test/petal.js
+++ test/petal.js
@@ -1,7 +1,7 @@
 const { Petal } = require('../dist');
 
 new Petal({
-    token: 'your token here!',
+    token: 'ODQ4NzY4NDg0Njg1OTA1OTUw.YLRbQA.6q4YlDeXDFMJQXv2ouNdl71yZYI',
     module_location: require('path').join(__dirname, 'modules'),
     database_location: require('path').join(__dirname, 'petal.sqlite')
 })
